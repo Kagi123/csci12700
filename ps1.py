f74a42f8c882555c70ca2f7f41f39fdd62c0a692
sangheum <68062948+Kagi123@users.noreply.github.com>
2022-02-02 19:51:37 -0500

    Add files via upload

---
+++ ps1.py
@@ -0,0 +1,6 @@
+#Name: Sangheum Park
+#Email:sangheum.park27@myhunter.cuny.edu
+#Date: January 31, 2022
+#This program prints: Hello, World!
+
+print("Hello, World!")
---
+++ ps2.py
@@ -0,0 +1,14 @@
+#Name: Sangheum Park
+#Email:sangheum.park27@myhunter.cuny.edu
+#Date: January 31, 2022
+#This program draws a octagon.
+
+import turtle
+wn = turtle.Screen()
+
+thomasH = turtle.Turtle()
+for i in range(8):
+    thomasH.forward(50)
+    thomasH.left(45)
+
+wn.exitonclick()
