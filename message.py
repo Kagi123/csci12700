ea415b812c3bfe9768cbbc21548d719c505afe6d
sangheum <68062948+Kagi123@users.noreply.github.com>
2022-02-04 22:58:14 -0500

    Add files via upload

---
+++ message.py
@@ -0,0 +1,11 @@
+#Name: Sangheum Park
+#Email:sangheum.park27@myhunter.cuny.edu
+#Date: February 04, 2022
+#This program write upper/lower message
+
+message = input("Enter a message: ")
+
+print(message)
+print(message.upper())
+print(message.lower())
+
