9ba0218f36ba0ce1cac61c03cea7728689b80514
sangheum <68062948+Kagi123@users.noreply.github.com>
2022-02-12 22:51:40 -0500

    Add files via upload

---
+++ colorbyhex.py
@@ -0,0 +1,12 @@
+#Name:Sangheum Park
+#Email:sangheum.park27@myhunter.cuny.edu
+#Date: February 12 2022
+#A program that demonstrates the shades of blue
+
+import turtle
+
+thea = turtle.Turtle()
+thea.shape("turtle")
+color = input("Enter a Hex string: ")
+
+thea.color(color)
