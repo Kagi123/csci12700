782be532ee615a9f7810290a5c1c9d106aa59549
unknown <spark26@angelo.edu>
2022-03-09 00:54:57 -0500

    update

--- red&white.py
+++ red&white.py
@@ -2,11 +2,19 @@
 #Email:sangheum.park27@myhunter.cuny.edu
 #Date: March 01, 2022
 #This program creates a image of red and white stripes. 
-
+import matplotlib.pyplot as plt
 import numpy as np
 
-num = int(input(’Enter size ’))
-img = np.ones( (num,num,3) )
+num = int(input("Enter size: "))
+name = input('Enter output file: ')
+
+img = np.ones((num,num,3))
 img[::2,:,1:] = 0
+
+
+plt.imsave(name,img)
+plt.show()
+
+
 
 
